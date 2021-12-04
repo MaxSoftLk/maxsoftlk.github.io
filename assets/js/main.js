4b0567380329d447f013541f5c22c7aca4745b45
Osanda Deshan <osanda.deshan@gmail.com>
2021-12-04 20:01:56 +0530

    Added 2 testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,11 +74,11 @@
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
-      } else {
-        selectHeader.classList.remove('header-scrolled')
-      }
+      // if (window.scrollY > 100) {
+      //   selectHeader.classList.add('header-scrolled')
+      // } else {
+      //   selectHeader.classList.remove('header-scrolled')
+      // }
     }
     window.addEventListener('load', headerScrolled)
     onscroll(document, headerScrolled)
